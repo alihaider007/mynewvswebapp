e3f9fa5be00c51569b5e0b45b8abd397d0c93974
Ali Haider <alihaidder_pilot@hotmail.com>
2018-09-11 23:13:58 +1000

    Add project files.

---
+++ WebApplication1/Pages/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+        private ILogger<CreateModel> Log;
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public CreateModel(AppDbContext db, ILogger<CreateModel> log)
+        {
+            this._db = db;
+            Log = log;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _db.Customer.Add(Customer);
+            await _db.SaveChangesAsync();
+
+            var msg = $"Customer {Customer.Name} added.";
+
+            Log.LogCritical(msg);
+
+            Message = msg;
+
+            return RedirectToPage("/Index");
+        }
+    }
+}
---
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppDbContext _db;
+        [BindProperty]
+        public IList<Customer> Customers { get; private set; }
+
+        [TempData]
+        public string Message { get; set; }
+
+        public IndexModel(AppDbContext db)
+        {
+            this._db = db;
+        }
+
+        public async Task OnGetAsync()
+        {
+            this.Customers = await this._db.Customer.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var customer = await _db.Customer.FindAsync(id);
+
+            if(customer != null)
+            {
+                _db.Customer.Remove(customer);
+                await _db.SaveChangesAsync();
+            }
+            
+            return RedirectToPage();
+        }
+    }
+}
